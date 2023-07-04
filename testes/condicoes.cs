07087e325daec26e414f989e1b53a67fa2217d28
Conrado Vinha <conradovinha@gmail.com>
2023-07-03 23:38:04 -0300

    up

--- testes/condicoes.cs
+++ testes/condicoes.cs
@@ -17,3 +17,37 @@ Console.WriteLine("You do not havd sufficient priveleges");
 else if (!permisson.Contains("Admin") || (!permisson.Contains("Manager"))) {
     Console.WriteLine("You do not have sufficient privileges");
 }
+
+//segunda possibilidade
+/*
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+*/
+
