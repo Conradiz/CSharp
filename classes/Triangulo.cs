dd5357e026bebd9b9311a1903b2b33afe31884ac
Conrado Vinha <conradovinha@gmail.com>
2023-07-24 21:45:28 -0300

    ok

---
+++ classes/Triangulo.cs
@@ -0,0 +1,9 @@
+namespace PrimeiroProjeto
+{
+    internal class Triangulo //Classe
+    {
+        public double A; //Atributo
+        public double C;
+        public double B; //Atributo
+    }
+}
