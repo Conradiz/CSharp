39287cc4b3dd992da32f418d0d50f8baf27262ba
Conrado Vinha <conradovinha@gmail.com>
2023-07-25 19:08:18 -0300

    create exercicio01

---
+++ execicios/estruturaSequencial/exercicio01.cs
@@ -0,0 +1,4 @@
+int numeroUm = int.Parse(Console.ReadLine());
+int numeroDois = int.Parse(Console.ReadLine());
+int soma = numeroUm + numeroDois;
+Console.WriteLine($"A soma de {numeroUm} + {numeroDois} é igual a: {soma}");
