0008ec78488160a151dc19bb3be42afadbe146b8
Conrado Vinha <conradovinha@gmail.com>
2023-07-25 21:47:16 -0300

    create

---
+++ execicios/estruturaSequencial/exercicio06.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+namespace ExercicioSeis
+    {
+    class Program
+    {
+        public static void Main(string[] args){
+            double ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+        }
+    }
+}
