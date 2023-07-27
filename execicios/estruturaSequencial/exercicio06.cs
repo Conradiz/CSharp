5904197e30cb65f9d7ada82433ee04a46d03f723
Conrado Vinha <conradovinha@gmail.com>
2023-07-26 22:55:41 -0300

    Update exercicio06.cs

--- execicios/estruturaSequencial/exercicio06.cs
+++ execicios/estruturaSequencial/exercicio06.cs
@@ -9,8 +9,10 @@ namespace ExercicioSeis
             double ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             double ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//altura
 
-            double areaTrianguloRetangulo = (ladoA * ladoB) / 2;
-            double areaTrapezio = ()
+            double areaTrianguloRetangulo = (ladoA * ladoC) / 2;
+            double areaTrapezio = ((ladoA + ladoB) * (ladoC)) / 2;
+            Console.WriteLine($"Area do Triangulo Retangulo: {areaTrianguloretangulo.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Area do Trapezio: {areaTrapezio.ToString("F3", CultureInfo.InvariantCulture)}");
         }
     }
 }
