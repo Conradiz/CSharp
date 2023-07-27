dcc03264ad9a7344a20910ceafcdf026f6469320
Conrado Vinha <conradovinha@gmail.com>
2023-07-27 10:23:31 -0300

    Create exercico05.cs

---
+++ execicios/estruturaCondicional/exercico05.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ExercicioCinco
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o codigo de um item: ");
+            Console.WriteLine ("Codigo\n Especificacao\n");
+        }
+    }
+}
