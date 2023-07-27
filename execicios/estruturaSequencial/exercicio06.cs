92b320ff899a541decec435043804b5590248ce8
Conrado Vinha <conradovinha@gmail.com>
2023-07-26 22:46:41 -0300

    up exercicio06

--- execicios/estruturaSequencial/exercicio06.cs
+++ execicios/estruturaSequencial/exercicio06.cs
@@ -5,11 +5,12 @@ namespace ExercicioSeis
     class Program
     {
         public static void Main(string[] args){
-            double ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//base 
             double ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//altura
 
-            
+            double areaTrianguloRetangulo = (ladoA * ladoB) / 2;
+            double areaTrapezio = ()
         }
     }
 }
