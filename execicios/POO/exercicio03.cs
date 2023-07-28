7ad3b3c908f628dba70c42c92dec8e20215f3f14
Conrado Vinha <conradovinha@gmail.com>
2023-07-28 12:05:49 -0300

    arquivos para o exercicio03

---
+++ execicios/POO/exercicio03.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+
+namespace Exerciciotres
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            
+
+            Console.Write("Digite o nome do Aluno");
+
+
+        }
+    }
+}
