b1a400ca5221e84d921bbdbeab9095007fe35fdb
Conrado Vinha <conradovinha@gmail.com>
2023-07-28 12:01:36 -0300

    finish exercicio02

--- execicios/POO/exercicio02.cs
+++ execicios/POO/exercicio02.cs
@@ -6,7 +6,26 @@ namespace ExercicioDois
     class Program
     {
         public static void Main (string[] args){
-          
+          Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine("Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto:");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(f);
+
+            Console.Write($"Salario Liquido: {f.SalarioLiquido()}");
+            
+            Console.Write ("Digite a porcentagem para aumentar o salario: ");
+            double por = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(por);
+
+            Console.Write($"Dados atualizados: {f}");
+
+
         }
     }
 }
