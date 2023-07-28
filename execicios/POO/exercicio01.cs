bf1b95850cd3d546b07e21b7051b43758d9714d6
Conrado Vinha <conradovinha@gmail.com>
2023-07-27 22:52:08 -0300

    finish exercicio01

--- execicios/POO/exercicio01.cs
+++ execicios/POO/exercicio01.cs
@@ -1,1 +1,20 @@
 using System;
+using System.Globalization;
+
+namespace ExercicioCinco
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Retangulo  r = new Retangulo();
+            Console.WriteLine("Digite a Altura do Triangulo:");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a Largura do triangulo:");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Dimensoes do Triangulo: {r}");
+
+        }
+    }
+}
