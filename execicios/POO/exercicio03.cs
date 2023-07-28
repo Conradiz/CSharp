fab1259bf8a970b12aaf9e2b6b81c4aa1f416f96
Conrado Vinha <conradovinha@gmail.com>
2023-07-28 12:30:54 -0300

    finish exercicio 03

--- execicios/POO/exercicio03.cs
+++ execicios/POO/exercicio03.cs
@@ -6,9 +6,25 @@ namespace Exerciciotres
     {
         public static void Main (string[] args)
         {
-            
 
-            Console.Write("Digite o nome do Aluno");
+
+            Aluno al = new Aluno();
+
+           Console.Write("Digite o nome do aluno:");
+           al.Nome = Console.ReadLine();
+           Console.Write("Digite as tres notas do aluno");
+           al.NotaA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           al.NotaB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           al.NotaC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"NOTA FINAL = {al.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (al.Aprovacao()){
+                Console.Write("Aprovado");
+            }
+            else {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine($"Faltaram {al.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)} Pontos");
+            }
 
 
         }
