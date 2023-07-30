9952e381736f9a262888bcb37c997385947a9aa3
Conrado Vinha <conradovinha@gmail.com>
2023-07-29 22:55:05 -0300

    desafio 05

---
+++ execicios/MicrosoftLearn/desafio05.cs
@@ -0,0 +1,27 @@
+/* Desafio de código – Escrever um código para implementar as regras de jogo
+Veja as regras para o jogo de batalha que você precisa implementar em seu projeto de código:
+
+Você deve usar a instrução do-while ou a instrução while como um loop de jogo externo.
+O herói e o monstro começarão com dez pontos de integridade.
+Todos os ataques serão um valor entre 1 e 10.
+O herói atacará primeiro.
+Imprima a quantidade de integridade que o monstro perdeu e a integridade que resta a ele.
+Se a integridade do monstro for maior que zero, ele poderá atacar o herói.
+Imprima a quantidade de integridade que o herói perdeu e a integridade que resta a ele.
+Continue esta sequência de ataque até que a integridade do monstro ou do herói seja zero ou menos.
+Imprima quem foi o vencedor. */
+
+using System;
+using System.Globalization;
+
+namespace ExercicioQuatro
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
