8bee72abf02767f35a63894e5d1f0fd48ea7301a
Conrado Vinha <conradovinha@gmail.com>
2023-07-29 23:03:03 -0300

    Update desafio06.cs

--- execicios/MicrosoftLearn/desafio06.cs
+++ execicios/MicrosoftLearn/desafio06.cs
@@ -1,9 +1,21 @@
+/*A solução deve incluir uma iteração do-while ou while.
+
+Antes do bloco de iteração: a solução deve usar uma instrução Console.WriteLine() para solicitar ao usuário um valor inteiro entre 5 e 10.
+
+Dentro do bloco de iteração:
+
+A solução deve usar uma instrução Console.ReadLine() para obter uma entrada do usuário.
+A solução deve garantir que a entrada seja uma representação válida de um inteiro.
+Se o valor inteiro não estiver entre 5 e 10, o código deverá usar uma instrução Console.WriteLine() para solicitar ao usuário um valor inteiro entre 5 e 10.
+A solução deve garantir que o valor inteiro esteja entre 5 e 10 antes de sair da iteração.
+Abaixo (depois) do bloco de código de iteração: a solução deve usar uma instrução Console.WriteLine() para informar ao usuário que o valor de entrada foi aceito. */
+
 using System;
 
 namespace ExercicioSeis{
     class Program{
         public static void Main(string[] args){
-            
+
         }
     }
 }
