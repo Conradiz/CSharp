229a5868e2d90332d5831fbce8580443975e8588
Conrado Vinha <conradovinha@gmail.com>
2023-07-31 16:28:29 -0300

    Create desafio09.cs

---
+++ execicios/MicrosoftLearn/desafio09.cs
@@ -0,0 +1,9 @@
+/* Declare uma matriz e inicialize-a com os seguintes elementos: B123
+C234, A345, C15 ,B177, G3003, C235, B179
+Esses valores representam as IDs de pedidos fraudulentos que seu aplicativo usará.
+
+Crie uma instrução foreach para percorrer cada elemento da sua matriz.
+
+Reporte as IDs de pedido que começam com a letra "B".
+
+É necessário avaliar cada elemento da matriz. Reporte as IDs de pedido potencialmente fraudulentos detectando os pedidos que começam com a letra "B". Para determinar se um elemento começa ou não com a letra "B", use o método String.StartsWith(). */
