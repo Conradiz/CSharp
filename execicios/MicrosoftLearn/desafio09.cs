ee3e055848b1fb88a6cff61857bc6482a4663464
Conrado Vinha <conradovinha@gmail.com>
2023-07-31 22:19:19 -0300

    Update desafio09.cs

--- execicios/MicrosoftLearn/desafio09.cs
+++ execicios/MicrosoftLearn/desafio09.cs
@@ -7,3 +7,20 @@ Crie uma instrução foreach para percorrer cada elemento da sua matriz.
 Reporte as IDs de pedido que começam com a letra "B".
 
 É necessário avaliar cada elemento da matriz. Reporte as IDs de pedido potencialmente fraudulentos detectando os pedidos que começam com a letra "B". Para determinar se um elemento começa ou não com a letra "B", use o método String.StartsWith(). */
+using System;
+using System.Globalization;
+
+namespace DesafioNove
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            string[] ids = {"B123","C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+            foreach (string id in ids){
+                Console.WriteLine($"Pedidos com possiveis fraudes: {id}");
+            }
+        }
+    }
+}
