184748eb9d2b43f645bc29f56139781874bb3dca
Conrado Vinha <conradovinha@gmail.com>
2023-07-31 22:24:52 -0300

    Create exercicio01.cs

---
+++ execicios/for/exercicio01.cs
@@ -0,0 +1,13 @@
+//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+using System;
+
+namespace ExercicioUm
+{
+    class Program
+    {
+        public static void Main (string[] args )
+        {
+            int valor = int.Parse(Console.ReadLine());
+        }
+    }
+}
