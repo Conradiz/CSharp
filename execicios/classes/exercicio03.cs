12a94341f8860d07a6188aa5ccd708fb32765795
Conrado Vinha <conradovinha@gmail.com>
2023-08-02 14:50:17 -0300

    Update exercicio03.cs

--- execicios/classes/exercicio03.cs
+++ execicios/classes/exercicio03.cs
@@ -4,7 +4,16 @@ namespace ExercicioTres
     {
         public static void Main (string[] args)
         {
-            Retangulo l = new Retangulo();
+            Retangulo dimensoes = new Retangulo();
+            Console.Write("Digite a Largura do Retangulo: ");
+            dimensoes.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a Altura do Retangulo: ");
+            dimensoes.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine($"Area do Retangulo = {dimensoes.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Perimetro do Retangulo = {dimensoes.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Diagonal do Retangulo = {dimensoes.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+
         }
     }
 
