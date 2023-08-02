9590b01b82afff6b78e90bab3135364016dbc09c
Conrado Vinha <conradovinha@gmail.com>
2023-08-02 14:51:51 -0300

    Update Retangulo.cs

--- execicios/classes/Retangulo.cs
+++ execicios/classes/Retangulo.cs
@@ -5,5 +5,15 @@ namespace ExercicioCinco{
 
         public double Largura;
         public double Altura;
+        
+        public double Area(){
+            return Largura * Altura;
+        }
+        public double Perimetro(){
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal(){
+            return Math.Sqrt(((Altura * Altura) + (Largura * Largura))) ;
+        }
     }
 }
