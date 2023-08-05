d2241250a8bf88ee2d94f797083636a7c8da916b
Conrado Vinha <conradovinha@gmail.com>
2023-08-04 22:54:41 -0300

    Update ContaBancaria.cs

--- exemplos/Properties/exercicio/ContaBancaria.cs
+++ exemplos/Properties/exercicio/ContaBancaria.cs
@@ -3,5 +3,15 @@ namespace ConsoleApp
 {
     internal class ContaBancaria
     { 
+         private double _saldo;
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        
+        //Construtor Personalizado
+        public ContaBancaria(int numero, string titular, double saldo) { 
+                Numero = numero;
+                Titular = titular;
+                _saldo = saldo;
+        }
     }
 }
