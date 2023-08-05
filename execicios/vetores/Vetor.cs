fc03a7245c25db41a16e57d1cc6710aceedf8c9d
Conrado Vinha <conradovinha@gmail.com>
2023-08-04 23:06:03 -0300

    explicando vetores

---
+++ execicios/vetores/Vetor.cs
@@ -0,0 +1,14 @@
+//Em Programação, "vetor' é o nome dado a arranjos inidimensionais
+using System.Globalization;
+
+namespace Vetores {
+    static void Main (string[] args) {
+        int n = int.Parse(Console.ReadLine());
+
+        double [] vect = new double [n];
+
+        for (int i = 0; i < n; i++){
+            vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+    }
+}
