4605ff3868bb893e9bfd79e418fbb3ea16cc982a
Conrado Vinha <conradovinha@gmail.com>
2023-08-04 22:55:55 -0300

    Classe ContaBancaria Finalizada

--- exemplos/Properties/exercicio/ContaBancaria.cs
+++ exemplos/Properties/exercicio/ContaBancaria.cs
@@ -13,5 +13,30 @@ namespace ConsoleApp
                 Titular = titular;
                 _saldo = saldo;
         }
+         public double Saldo { 
+            get { return (double)_saldo; }
+            set
+            {
+                if ( value > 0 ) {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public void Deposito(double quantia)
+        {
+            _saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            _saldo -= quantia + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Numero
+            + ", Titular: " + Titular
+            + ", Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
